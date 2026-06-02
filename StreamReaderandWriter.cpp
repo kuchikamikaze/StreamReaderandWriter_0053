d920d5c67b83ddadf62b10b3a2fd0487245a6f5a
Naufal Arif <na713634@gmail.com>
2026-06-02 16:04:19 +0700

    input the data you want to save with looping

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -13,3 +13,17 @@ int main()
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+    }
